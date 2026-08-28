f197b1bcc21d3c4b9cdbe1974dd641c41a742ee6
Wildan Maulana <117628088+maulzz@users.noreply.github.com>
2026-08-28 19:00:55 +0700

    Add files via upload

---
+++ Pertemuan1/Pertemuan1.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pertemuan1;
+
+/**
+ *
+ * @author acer
+ */
+public class Pertemuan1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       String nama, alamat, no_hp;
+       long npm;
+       
+       nama = "Wildan Maulana H";
+       npm = 2505060022L;
+       alamat = "Magelang";
+       no_hp = "08526128374";
+       
+        System.out.println("Nama: " + nama);
+        System.out.println("NPM : " + npm);
+        System.out.println("Alamat : " + alamat);
+        System.out.println("No Hp : " + no_hp);
+       
+    }
+    
+}
